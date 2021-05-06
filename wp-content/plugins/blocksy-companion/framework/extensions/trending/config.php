a46ccfe2b763c1fd7fc04c3c96fcc2c19f5e6cfe
jcemino <30559329+jcemino@users.noreply.github.com>
2021-05-06 21:39:52 +0800

    w

--- wp-content/plugins/blocksy-companion/framework/extensions/trending/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$config = [
-	'name' => __('Trending Posts', 'blc'),
-	'description' => __('Display a trending list of posts, products or custom post types at the bottom of your website.', 'blc')
-];
--- wp-content/plugins/blocksy-companion/static/js/sticky.js
+++
@@ -1,9 +0,0 @@
-import ctEvents from 'ct-events'
-import { registerDynamicChunk } from 'blocksy-frontend'
-import { mountStickyHeader } from './frontend/sticky'
-
-mountStickyHeader()
-
-registerDynamicChunk('blocksy_sticky_header', {
-	mount: (el) => {},
-})
